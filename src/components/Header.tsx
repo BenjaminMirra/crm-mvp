0bfe562c8fa689c9f653f8b19a36cd083a288727
BenjaminMirra <mirrabenjamin@gmail.com>
2025-11-02 10:36:50 -0300

    fixs

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,7 +10,7 @@ const Header: React.FC<HeaderProps> = ({ children }) => {
     <AppBar position="static">
       <Toolbar sx={{ gap: 2 }}>
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          Dashboard de Finanzas
+          Dashboard - CRM
         </Typography>
         {children && <Box>{children}</Box>}
       </Toolbar>
